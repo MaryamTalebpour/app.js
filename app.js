402296393d0f4cf898334082d9b7ad8928ad5983
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-06 10:22:00 +0330

    .

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let firstName = 'Maryam'
 let lastName = 'Talebpour'
 
-let fullName = firstName + ' ' + lastName
+//let fullName = firstName + ' ' + lastName
 
-console.log(fullName)
+//console.log(fullName)
+
+let num1 = 12.4
+let num2 = -4
+let num3 = 20
+let sum = num1 + num2 / num3
+
+console.log(sum)

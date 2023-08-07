978417b0d21b01f476337f27c2731616d34de3f1
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-07 12:31:11 +0330

    .

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 let firstName = 'Maryam'
-
-if (true) {
+ if (true) {
     let lastName = 'Talebpour'
     console.log(lastName)
-    console.log(firstName)
-}
+    if (true) {
+        let age = 30
+        console.log(age)
+    }
+ }
 
-console.log(firstName)
-//console.log(lastName) wrong!
+ console.log(firstName)
+
+ let name = 'test1'
+  if (true) {
+    let name = 'test2'
+    if (true) {
+        let name = 'test3'
+        console.log(name)
+    }
+  }
+
+  console.log(name)

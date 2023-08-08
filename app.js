4bfed8270b31d9e0c7e5903dffa8931787db351b
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 20:52:26 +0330

    .

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cartItems = ['book1', 'book2', 'book3' , 'book4']
 
-cartItems.splice(0, 2, 'newitem')
-
-cartItems[5] = 'book5'
-
-console.log(cartItems)
+cartItems.forEach( function(item, index) {
+    console.log(item)
+    console.log(index)
+})

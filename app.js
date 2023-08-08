d059fd783868d0da665fbae9a741bd60d0933353
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 17:41:29 +0330

    .

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let isValidPassword = function(password) {
-    if (password.length > 8 && !password.includes('12345')) {
-        return true
-    } else {
-        return false
-    }
-}
+let num = 12.56789
 
-console.log(isValidPassword('manjdowldf'))
-console.log(isValidPassword('12345'))
-console.log(isValidPassword('maryfj'))
+let res = num.toFixed(3)
+console.log(res)
+
+console.log(num.toFixed(1))
+
+console.log(Math.round(num))
+
+console.log(Math.floor(num))
+
+console.log(Math.ceil(num))
+
+let randomNum = Math.random()
+
+console.log(randomNum)
+
+let num1 = 10
+let num2 = 20
+
+let randomNums = Math.floor(Math.random() * (num2 - num1)) +10
+
+console.log(randomNums)

51f6c359d2e548559b4be4705a3a80b39d21c7ff
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 17:49:39 +0330

    .

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-let num = 12.56789
+const userInfo = {
+    id : 2
+}
 
-let res = num.toFixed(3)
-console.log(res)
+userInfo.id = 3
 
-console.log(num.toFixed(1))
-
-console.log(Math.round(num))
-
-console.log(Math.floor(num))
-
-console.log(Math.ceil(num))
-
-let randomNum = Math.random()
-
-console.log(randomNum)
-
-let num1 = 10
-let num2 = 20
-
-let randomNums = Math.floor(Math.random() * (num2 - num1)) +10
-
-console.log(randomNums)
+console.log(userInfo.id)

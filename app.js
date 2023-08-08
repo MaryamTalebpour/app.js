d16ce0350eca5a00f8cd44115e30c626e3745d37
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 13:47:55 +0330

    .

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-let fullName = 'MaryamTalebpour'
+let isValidPassword = function(password) {
+    if (password.length > 8 && !password.includes('12345')) {
+        return true
+    } else {
+        return false
+    }
+}
 
-console.log(fullName.length)
-console.log(fullName.toUpperCase())
-
-let password = 'Maryam12345'
-
-console.log(password.includes('123459'))
-
-let email = ' maryam@gmail.com'
-
-console.log(email)
-
-console.log(email.trim())
+console.log(isValidPassword('manjdowldf'))
+console.log(isValidPassword('12345'))
+console.log(isValidPassword('maryfj'))

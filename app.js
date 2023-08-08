4279ddde239fca09becbdf2a79768c2fbea69519
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 18:59:34 +0330

    .

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const userInfo = {
-    id : 2
-}
+const tasks = ['task1', 'task2', 'task3', 12, true]
 
-userInfo.id = 3
+console.log(tasks)
+console.log(tasks.length)
+console.log(tasks[4])
 
-console.log(userInfo.id)
+
+const cartItems = ['book1', 'book2', 'book3']
+
+let res = `you have ${cartItems.length} items in your cart`
+
+console.log(res)
+
+console.log(`first item is : ${cartItems[0]} - last item is : ${cartItems[cartItems.length-1]}`)

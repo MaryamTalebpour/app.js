77625fbe598feafd8b02059b531f95310cce4194
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-08 19:53:43 +0330

    .

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const tasks = ['task1', 'task2', 'task3', 12, true]
+const cartItems = ['book1', 'book2', 'book3' , 'book4']
 
-console.log(tasks)
-console.log(tasks.length)
-console.log(tasks[4])
+cartItems.push('book5')
+cartItems.pop()
 
-
-const cartItems = ['book1', 'book2', 'book3']
-
-let res = `you have ${cartItems.length} items in your cart`
-
-console.log(res)
-
-console.log(`first item is : ${cartItems[0]} - last item is : ${cartItems[cartItems.length-1]}`)
+cartItems.unshift('first item')
+cartItems.shift()
+console.log(cartItems)

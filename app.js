17cec1b31818939bde892b52f4fd0460d3d41c72
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-10 13:44:14 +0330

    .

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const cartItems =[ {
+    title :'book1',
+     price : 20
+} , {
+    title :'book2',
+    price :30
+} , {
+    title :'book3',
+    price : 40
+} , {
+    title :'book4',
+    price :50
+}]
 
-const cartItem = ['book1', 'book2' , 'book3', 'book4']
+const indexValue = cartItems.findIndex( function(item, index){
+    return item.title === 'book3'
+})
 
-console.log(cartItem.indexOf('book3'))
-
-console.log(cartItem.indexOf('maryam'))
+console.log(indexValue)

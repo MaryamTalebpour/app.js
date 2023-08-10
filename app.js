12cd75a5658a2bbe5355f97c3ce1316f652ae021
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-10 12:23:37 +0330

    .

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-for (let count = 0; count <=5; count = count +1) {
-    console.log(count)
-}
-
-//count++  count= count+1
 
 const cartItem = ['book1', 'book2' , 'book3', 'book4']
 
-for (let count =0 ; count < cartItem.length ; count++) {
-    console.log(cartItem[count])
-}
+console.log(cartItem.indexOf('book3'))
 
-const cartItems = ['book1', 'book2' , 'book3' , 'book4']
-
-for (let count = 0; count < cartItems.length; count++){
-    const num = count +1
-    const productName = cartItems[count]
-    console.log(`${num} - product Name : ${productName}`)
-}
+console.log(cartItem.indexOf('maryam'))

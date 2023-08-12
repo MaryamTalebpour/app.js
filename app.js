0275f6f434c59527fc6fd46a92118653bc6d03a6
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-12 20:52:51 +0330

    .

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const productItems =[ {
-    title :'book1',
-     exist : true
-} , {
-    title :'book2',
-    exist : false
-} , {
-    title :'book3',
-    exist : true
-} , {
-    title :'book4',
-    exist : false
-}]
-
-const sortProduct = function(products) {
-    products.sort(function(a , b) {
-        if(a.exist === true && b.exist === false){
-            return -1
-        } else if(b.exist === true && a.exist ===false){
-            return 1
-        }else {
-            return 0
-        }
-    })
+const account = {
+    name : 'Maryam',
+    income : [],
+    outgo : [],
+    addOutgo : function(description, amount){
+        this.outgo.push({
+            description : description,
+            amount : amount
+        })
+    },
+    addIncome : function(description, amount) {
+        this.income.push({
+            description : description,
+            amount : amount
+        })
+    },
+    getAccountSummary : function(){
+        let totalOutgo = 0
+        let totlaIncome = 0
+        let accountBalance =0
+        this.income.forEach(function(item){
+            totlaIncome = totlaIncome + item.amount
+        }),
+        this.outgo.forEach(function(item){
+            totalOutgo = totalOutgo + item.amount
+        }),
+        accountBalance = totlaIncome - totalOutgo
+        return (
+            `${this.name} - balance : ${accountBalance}- income : ${totlaIncome} - outgo : ${totalOutgo}`
+        )    
+    }
 }
 
-sortProduct(productItems)
-console.log(productItems)
+
+
+account.addOutgo('book', 200)
+account.addOutgo('cofe', 300)
+account.addIncome('job', 1000)
+console.log(account.getAccountSummary())

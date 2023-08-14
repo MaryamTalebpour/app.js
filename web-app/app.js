ab1118bf095f93ba80542068d1926997d30ae05d
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-14 21:50:51 +0330

    .

--- web-app/app.js
+++ web-app/app.js
@@ -1,7 +1,27 @@
-document.querySelector('#search-products').addEventListener('change', function(e){
-   console.log(e.target.value)
-})
+const products = [{
+   title : 'Node.js Design Patterns'
+}, {
+   title : 'you dont no js '
+}, {
+   title : 'Functional React'
+}, {
+   title: 'you dont know js'
+}]
+
+const filters = {
+   searchItems : ''
+}
+
+const renderProducts = function(products, filters){
+   const filterProducts = products.filter(function(item){
+      return item.title.toLowerCase().includes(filters.searchItems.toLowerCase())
+   })
+   console.log(filterProducts)
+}
+
+renderProducts(products, filters)
 
 document.querySelector('#search-products').addEventListener('input', function(e){
-   console.log(e.target.value)
+   filters.searchItems = e.target.value
+   renderProducts(products, filters)
 })

32e0a6d5dae4e3888950903dadbafe7a7ab1dd25
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-14 13:50:19 +0330

    .

--- web-app/app.js
+++ web-app/app.js
@@ -1,9 +1,7 @@
-document.querySelector('#add-product').addEventListener('click', function(e){
-   console.log('product created!')
+document.querySelector('#search-products').addEventListener('change', function(e){
+   console.log(e.target.value)
 })
 
-document.querySelector('#Remove-product').addEventListener('click', function(e){
-   document.querySelectorAll('.product').forEach(function(item){
-      item.remove()
-   })
+document.querySelector('#search-products').addEventListener('input', function(e){
+   console.log(e.target.value)
 })

e76266a98cc86340a38c67294422acb02a8753de
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-14 11:37:40 +0330

    .

--- web-app/app.js
+++ web-app/app.js
@@ -1,28 +1,5 @@
-const productItems = [{
-   title : 'book1',
-   exist : true
-}, {
-   title : 'book2',
-   exist : false
-} , {
-   title : 'book3',
-   exist : true
-} , {
-   title : 'book4',
-   exist : false
-}]
-
-
-const availableProduct = productItems.filter(function(item){
-   return item.exist === true
-})
-
-const message = document.createElement('h2')
-message.textContent = `number of available products : ${availableProduct.length}`
-document.querySelector('body').appendChild(message)
-
-productItems.forEach(function(item){
-   const p = document.createElement('p')
-   p.textContent = item.title
-   document.querySelector('body').appendChild(p)
+document.querySelector('button').addEventListener('click', function(e){
+   //console.log('event')
+   //console.log(e)
+   e.target.textContent = 'New Value'
 })

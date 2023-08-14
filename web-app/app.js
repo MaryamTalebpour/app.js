1315ab47ecafefa119aab5411822c9deb9ccdb25
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-14 11:02:17 +0330

    .

--- web-app/app.js
+++ web-app/app.js
@@ -1,3 +1,28 @@
-const newP = document.createElement('p')
-newP.textContent = 'new paragraph'
-document.querySelector('body').appendChild(newP)
+const productItems = [{
+   title : 'book1',
+   exist : true
+}, {
+   title : 'book2',
+   exist : false
+} , {
+   title : 'book3',
+   exist : true
+} , {
+   title : 'book4',
+   exist : false
+}]
+
+
+const availableProduct = productItems.filter(function(item){
+   return item.exist === true
+})
+
+const message = document.createElement('h2')
+message.textContent = `number of available products : ${availableProduct.length}`
+document.querySelector('body').appendChild(message)
+
+productItems.forEach(function(item){
+   const p = document.createElement('p')
+   p.textContent = item.title
+   document.querySelector('body').appendChild(p)
+})

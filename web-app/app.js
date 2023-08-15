dfa69ab290e7340a3bd3a88589d02aa9305d987e
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-08-15 13:37:34 +0330

    .

--- web-app/app.js
+++ web-app/app.js
@@ -1,32 +1,5 @@
-const products = [{
-   title : 'Node.js Design Patterns'
-}, {
-   title : 'you dont no js '
-}, {
-   title : 'Functional React'
-}, {
-   title: 'you dont know js'
-}]
-
-const filters = {
-   searchItems : ''
-}
-
-const renderProducts = function(products, filters){
-   const filterProducts = products.filter(function(item){
-      return item.title.toLowerCase().includes(filters.searchItems.toLowerCase())
-   })
-   document.querySelector('#products').innerHTML = ''
-   filterProducts.forEach( function(item){
-      const productEl = document.createElement('p')
-      productEl.textContent = item.title
-      document.querySelector('#products').appendChild(productEl)
-   })
-}
-
-renderProducts(products, filters)
-
-document.querySelector('#search-products').addEventListener('input', function(e){
-   filters.searchItems = e.target.value
-   renderProducts(products, filters)
+document.querySelector('#Add-product').addEventListener('submit', function(e){
+   e.preventDefault()
+   console.log(e.target.elements.product.value)
+   e.target.elements.product.value = ''
 })
